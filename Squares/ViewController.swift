85c2bf4ca7edf6bb472ab10def7af4e5e1111597
LyakhovYA <lyakhovya@yandex.ru>
2020-05-20 16:39:07 +0300

    Initial Commit

---
+++ Squares/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Squares
+//
+//  Created by Юрий Ляхов on 20.05.2020.
+//  Copyright © 2020 Yuri Lyakhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
